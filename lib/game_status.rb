29ecdb5b360d88f9c8ec06368fa7d0646c1df38e
Gaida M Escobar <gescobar129@gmail.com>
2019-08-25 18:11:33 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,23 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || full?(board)
+end
+
+def winner(board)
+  if won?(board)
+    winning_combo = won?(board)
+    bard[winning_combo.first]
+  end
+end
